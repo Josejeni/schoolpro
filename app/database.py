96675960f9a0036fdb35af4363dc443b6ed44ff2
josephin <josephinjenifer373@gmail.com>
2022-09-13 10:11:03 +0530

    missing

--- app/database.py
+++ app/database.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
+
+
+print(settings.database_hostname)
 engine=create_engine(f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
 Sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
