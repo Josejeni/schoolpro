686bc825b379f9cef0bd2be1ab257a173b533b00
josephin <josephinjenifer373@gmail.com>
2022-09-13 09:59:40 +0530

    commit for db

--- app/config.py
+++ app/config.py
@@ -7,7 +7,6 @@ class Settings(BaseSettings):
     database_name:str
     database_username:str
     database_password:str
-    
     class Config:
         env_file= ".env"
 
--- app/database.py
+++ app/database.py
@@ -3,6 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-engine=create_engine(f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:5432/{settings.database_name}')
+engine=create_engine(f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
 Sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
