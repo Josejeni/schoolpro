66f3d6acafb16edf0c4c60b44f538c07cbcc4bc3
josephin <josephinjenifer373@gmail.com>
2022-09-16 10:32:29 +0530

    secrets

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,9 +4,9 @@ from jose import jwt
 from app.config import settings
 from app import file1
 
-# def test_root(client):
-#    res= client.get("/")
-#    print(res.json())
+def test_root(client):
+   res= client.get("/")
+   print(res.json())
 
 
 
@@ -16,11 +16,9 @@ def test_login(client,test_register):
     res=client.post("/log_encrypt",data={"username":test_register['user_name'],"password":test_register['password']})
     print(res.json())
     res_token_detail = file1.token(** res.json())
-    # token = res.json().get('access_token')
-    # token_type = res.json().get('token_type')
     payload=jwt.decode(res_token_detail.access_token,settings.secret_key,settings.algorithm)
     username=payload.get('user_name')
     print(username)
     assert username == test_register['user_name']
-    # assert token_type == "bearer"
+  
 
